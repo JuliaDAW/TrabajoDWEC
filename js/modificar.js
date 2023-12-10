6857dff6d6cc30dae6748bf5cf852a14e79de21e
Fernando <ferestga8@gmail.com>
2023-12-10 11:45:44 +0100

    Arreglado bug al eliminar

--- js/modificar.js
+++ js/modificar.js
@@ -4,12 +4,9 @@ if(!window.indexedDB){
     console.log("Tu navegador no soporta IndexedDB");
 }
 
-let aniadir=document.getElementById("id_aniadir");
 let modificar=document.getElementById("id_modificar");
-
 // Abrir o crear la base de datos
 const request = window.indexedDB.open("BD_Practica_5", 3);
-
 //Apertura fallida
 request.onerror = function (event) {
     console.log("Error al intentar abrir la base de datos", event);
@@ -25,7 +22,7 @@ request.onsuccess = function (event) {
     
     modificar.addEventListener("submit", (event) => { //botón modificar
         event.preventDefault();
-
+        if(!modificar.elegir.value) return;
         let transaction = db.transaction(["platos"], "readwrite");
         let objectStore = transaction.objectStore("platos");
 
@@ -48,19 +45,18 @@ request.onsuccess = function (event) {
             });
 
             modificar.reset();
-            // Volver a rellenar el select
             select();
         }
     });
 
         modificar.elimina.addEventListener("click", (event)=>{ //botón eliminar
             event.preventDefault();
+            if(!modificar.elegir.value) return;
 
             let transaction1 = db.transaction(["platos"], "readwrite");
             let objectStore1 = transaction1.objectStore("platos");
 
-            objectStore1.delete(modificar.elegir.value); //elimina datos
-            select();
+            objectStore1.delete(parseInt(modificar.elegir.value)); //elimina datos
             
             transaction1.oncomplete = function(event) { //transacción exitosa
                 if(jsConfetti) jsConfetti.clearCanvas();
@@ -71,6 +67,7 @@ request.onsuccess = function (event) {
                 });
 
                 modificar.reset();
+                select();
             }
         });
     function select(){
@@ -78,24 +75,30 @@ request.onsuccess = function (event) {
         let objectStore = transaction.objectStore("platos");
         let cursor = objectStore.openCursor();
         let array=[];
+        disableForm();
         cursor.onerror = function(event) {
             console.log(event.target.errorCode)
         }
         cursor.onsuccess = function(event){
             let datos = event.target.result;
-            console.log("Entrando cursor")
+            console.log("Continuando cursor...")
             if(datos){
                 array.push(datos.value);
                 datos.continue();
             } else{
                 let elegir=document.getElementById("id_elegir");
-                elegir.innerHTML="<option>--Seleccione una opción--</option>";
+                elegir.innerHTML="<option value=''>--Seleccione una opción--</option>";
                 array.forEach(comida =>{
                     elegir.innerHTML+=`<option value=${comida.id}>${comida.nombre}</option>`;
                 });
 
-                elegir.addEventListener("change", (event)=>{
+                elegir.addEventListener("change", (event) => {
                     let aux=event.target.value;
+                    if(!aux) {
+                        disableForm();
+                        modificar.reset();
+                    return;
+                    }
                     console.log(array);
         
                     let encontrada=array.find(comida=>{
@@ -122,13 +125,13 @@ request.onsuccess = function (event) {
     }
 }
 
-
 // Este evento solo se ejecuta en la primera creación de la base de datos o en el cambio de versión
 request.onupgradeneeded = function (event) {
     console.log("Actualizando la base de datos");
     const db = event.target.result;
 
-    if(db.objectStoreNames.contains("platos")){ //si existe la elimina
+    //si existe la elimina, alternativamente se podría hacer una copia y modificar los datos para introducirlos en la nueva
+    if(db.objectStoreNames.contains("platos")){
         db.deleteObjectStore("platos");
     }
     // Crear un almacén de objetos para esta base de datos
@@ -138,4 +141,13 @@ request.onupgradeneeded = function (event) {
     objectStore.createIndex("nombre", "nombre", { unique: true });
     objectStore.createIndex("precio", "precio", { unique: false });
     objectStore.createIndex("ingredientes", "ingredientes", { unique: false });
+}
+
+function disableForm() {
+    modificar.nombre.disabled=true;
+    modificar.precio.disabled=true;
+    modificar.ingredientes.disabled=true;
+    modificar.image.disabled=true;
+    modificar.modifica.disabled=true;
+    modificar.elimina.disabled=true;
 }
